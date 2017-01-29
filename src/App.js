7985ee660b163aac7512c123b80115181c5d6f15
jcvila <jcvilap@gmail.com>
2017-01-28 20:26:55 -0500

    Adding Material UI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import './App.css';
+import logo from './logo.svg';
 
 export class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(){
+        super();
+
+        // Needed for onTouchTap
+        injectTapEventPlugin();
+    }
+
+    getChildContext() {
+        return { muiTheme: getMuiTheme(baseTheme) };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <AppBar
+                    title="Have a nice Shopping!"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                />
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+
+    static get childContextTypes(){
+        return { muiTheme: React.PropTypes.object.isRequired };
+    }
 }
