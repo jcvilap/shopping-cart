83d11c051cde2bb97631bdb6994cc340dd7155ba
jcvila <jcvilap@gmail.com>
2017-02-08 19:30:55 -0500

    Adding GridList Component

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,35 +1,63 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import axios from 'axios';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import store from '../../store';
 import {PRODUCT_URL} from './constants';
+import {styles} from './styles';
 
 class ProductList extends React.Component {
     render() {
         let template = (
             <div>error</div>
         );
-        if(this.props.products.fetching) {
-            template =  (
+        if (this.props.products.fetching) {
+            template = (
                 <div>loading</div>
             );
         } else {
             this.data = this.props.products.data;
-            template = (
-                <div>{this.renderProducts(this.data.products)}</div>
-            );
+            template = this.getTemplate(this.data.products);
         }
 
         return template;
     }
 
-    renderProducts (products) {
+    renderProducts(products) {
         let count = 0;
         return products.map(product => {
             return (
-                <div key={count++}>{JSON.stringify(product.toString())}</div>
+                <div key={count++}>{JSON.stringify(product)}</div>
             );
         });
+    }
+
+    getTemplate(data) {
+        return (
+            <div style={styles.root}>
+                <GridList
+                    cols={4}
+                    cellHeight={200}
+                    padding={1}
+                    style={styles.gridList}>
+                    {data.map((tile) => (
+                        <GridTile
+                            key={tile.image}
+                            title={tile.name}
+                            actionIcon={<IconButton><StarBorder color="white"/></IconButton>}
+                            actionPosition="left"
+                            titlePosition="top"
+                            titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                            cols={tile.featured ? 2 : 1}
+                            rows={tile.featured ? 2 : 1}>
+                            <img src={tile.image}/>
+                        </GridTile>
+                    ))}
+                </GridList>
+            </div>
+        );
     }
 
     static getProducts() {
---
+++ src/components/ProductList/styles.js
@@ -0,0 +1,15 @@
+/**
+ * Created by juliocvila on 2/8/17.
+ */
+export const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    gridList: {
+        width: 500,
+        height: 450,
+        overflowY: 'auto',
+    },
+};
