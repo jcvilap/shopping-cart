f9aa59477bd35441db15c682c35d6dec0d9ecfd5
jcvilap <jcvilap@gmail.com>
2017-03-07 13:43:35 -0500

    Adding reducer for the shopping cart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -4,13 +4,13 @@ export default (state = new Map(), action) => {
             let i = state.get(action.payload.sku) || {value: action.payload, count: 0};
             i.count++;
             state.set(i.value.sku, i);
-            return Object.assign({}, state);
+            return new Map(state);
         case 'REMOVE_ITEM_TO_CART':
             let item = state.get(action.payload.sku) || {value: action.payload, count: 0};
             item.count && item.count--;
             state.set(item.value.sku, item);
-            return Object.assign({}, state);
+            return new Map(state);
         default:
-            return state;
+            return new Map(state);
     }
 }
